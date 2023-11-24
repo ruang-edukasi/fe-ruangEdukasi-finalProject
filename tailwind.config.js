536ee0cb24afd0f8364bc455426a235093a60dbe
Ahmaddds <82545728+Ahmadzeeyy@users.noreply.github.com>
2023-11-24 19:56:36 +0700

    Ahmad (#3)
    * Add : Pages
    
    * fixed tailwind
    
    * fix: route
    
    * Fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#6148FF",
-      alert: "#252D3A",
-      succes: "#DF9947",
+    extend: {
+      colors: {
+        primary: "#6148FF",
+        alert: "#252D3A",
+        succes: "#DF9947",
+      },
     },
   },
   plugins: [],
