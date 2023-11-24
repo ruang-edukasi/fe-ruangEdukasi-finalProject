09e6ccf36f3c9ee30c9358c8b6eda7472de2a6b1
Ahmads <bariziahnad19@gmail.com>
2023-11-24 13:09:59 +0700

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Login";
@@ -7,17 +6,15 @@ import Register from "./Pages/Register";
 
 function App() {
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Routes>
         <Route path="/login" element={<Login />} />
-      </Routes>
-      <Routes>
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
+    
   );
 }
 
