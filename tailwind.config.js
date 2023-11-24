4d45879fdf677725434b2334032bd7ac5266584d
Ahmaddds <82545728+Ahmadzeeyy@users.noreply.github.com>
2023-11-24 12:54:22 +0700

    Ahmad (#1)
    * Add : Pages
    
    * fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#6148FF",
+      alert: "#252D3A",
+      succes: "#DF9947",
+    },
   },
   plugins: [],
 };
