d09332cf05e00a1e5fe1242cab7ddf26b66ab4f6
Ariantika Putri Maharani <120215554+arntka110@users.noreply.github.com>
2023-12-02 01:04:33 +0800

    update: redux for show profile (#19)
    * Add : Pages
    
    * fixed tailwind
    
    * add feature: home page
    
    * update component
    
    * update: redux for show profile
    
    ---------
    
    Co-authored-by: Ahmads <bariziahnad19@gmail.com>

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,3 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faShieldHeart,
+  faStar,
+  faBook,
+  faClock,
+  faGem,
+} from "@fortawesome/free-solid-svg-icons";
+
 function Card() {
   return (
     <>
@@ -11,17 +20,51 @@ function Card() {
           <div className="p-4">
             <div className="flex justify-between">
               <h3 className="text-primary font-bold">UI/UX Design</h3>
-              <p className="font-bold">⭐4.7</p>
+              <p className="font-bold">
+                <span>
+                  <FontAwesomeIcon icon={faStar} className="text-amber-400" />
+                </span>
+                4.7
+              </p>
             </div>
             <h3 className="font-bold">Belajar Web Designer dengan figma</h3>
             <p>by Angela Doe</p>
-            <div className="flex justify-between font-semibold">
-              <p className="text-primary">Intermediate Level</p>
-              <p>10 Modul</p>
-              <p>120 Menit</p>
+            <div className="flex justify-between font-semibold text-sm">
+              <p className="text-primary">
+                <span>
+                  <FontAwesomeIcon
+                    icon={faShieldHeart}
+                    className="text-success mr-1"
+                  />
+                </span>
+                Intermediate Level
+              </p>
+              <p>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faBook}
+                    className="text-success mr-1"
+                  />
+                </span>
+                10 Modul
+              </p>
+              <p>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faClock}
+                    className="text-success mr-1"
+                  />
+                </span>
+                120 Menit
+              </p>
             </div>
-            <button className="inline-flex mt-2 h-7 w-44 px-5 items-center justify-center rounded-2xl bg-blue-500 text-sm font-medium text-white transition duration-300 hover:bg-blue-800">
-              <p className="pr-4">💎Beli</p>
+            <button className="inline-flex mt-2 h-7 w-44 px-5 items-center justify-between rounded-2xl bg-blue-500 text-sm font-medium text-white transition duration-300 hover:bg-blue-800">
+              <p>
+                <span>
+                  <FontAwesomeIcon icon={faGem} className="text-white mr-1" />
+                </span>
+                Beli
+              </p>
               <p>Rp 249.000</p>
             </button>
           </div>
