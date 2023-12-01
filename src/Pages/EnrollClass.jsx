1df590f97682238f532be33bf2ba7dacd6c9e89f
Ahmaddds <82545728+Ahmadzeeyy@users.noreply.github.com>
2023-12-01 17:45:11 +0700

    Ahmad (#16)
    * Add : Pages
    
    * fixed tailwind
    
    * fix: route
    
    * Fix: tailwind config
    
    * add page login
    
    * Fixing : fixing bug visibility password
    
    * add: responsive

---
+++ src/Pages/EnrollClass.jsx
@@ -0,0 +1,172 @@
+import Header from "../Components/Header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import courseImage from "../assets/category1.svg";
+
+function EnrollClass() {
+  return (
+    <>
+      <Header />
+      <section>
+        <div className="container mx-auto px-24 content w-full flex flex-col py-6 border drop-shadow-lg ">
+          <Link to={"/"}>
+            <div className="text-base font-semibold leading-tight tracking-tight text-black md:text-2xl">
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                className="mr-6 text-black inline"
+              />
+              Kembali
+            </div>
+          </Link>
+          <div className="justify-self-center mx-auto bg-alert px-28 py-2 text-white rounded-lg">
+            Selesaikan Pembayaran sampai 10 Maret 2023 12:00
+          </div>
+        </div>
+        <div className="flex mx-auto px-24 mt-10 gap-10 border justify-end">
+          <div className="flex flex-col gap-2 w-8/12">
+            <div className="collapse collapse-arrow bg-gray-800 rounded-sm">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text- font-medium text-white ">
+                Bank Transfer
+              </div>
+              <div className="collapse-content bg-white">
+                <p>hello</p>
+              </div>
+            </div>
+            <div className="collapse collapse-arrow bg-primary rounded-sm">
+              <input type="radio" name="my-accordion-2" checked="checked" />
+              <div className="collapse-title text- font-medium text-white">
+                Bank Transfer
+              </div>
+              <div className="collapse-content bg-white ">
+                <form className="max-w-md mx-auto mt-10">
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="number"
+                      name="floating_email"
+                      id="floating_email"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none"
+                      placeholder=" "
+                      pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+                      required
+                    />
+                    <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto  peer-focus:scale-75 peer-focus:-translate-y-6">
+                      Card Number
+                    </label>
+                  </div>
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="password"
+                      name="floating_password"
+                      id="floating_password"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4  peer-focus:scale-75 peer-focus:-translate-y-6">
+                      Password
+                    </label>
+                  </div>
+                  <div className="relative z-0 w-full mb-5 group">
+                    <input
+                      type="password"
+                      name="repeat_password"
+                      id="floating_repeat_password"
+                      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4  peer-focus:scale-75 peer-focus:-translate-y-6">
+                      Confirm password
+                    </label>
+                  </div>
+                  <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 group">
+                      <input
+                        type="text"
+                        name="floating_first_name"
+                        id="floating_first_name"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 peer"
+                        placeholder=" "
+                        required
+                      />
+                      <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4  peer-focus:scale-75 peer-focus:-translate-y-6">
+                        First name
+                      </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                      <input
+                        type="text"
+                        name="floating_last_name"
+                        id="floating_last_name"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 peer"
+                        placeholder=" "
+                        required
+                      />
+                      <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4  peer-focus:scale-75 peer-focus:-translate-y-6">
+                        Last name
+                      </label>
+                    </div>
+                  </div>
+                  <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 group">
+                      <input
+                        type="tel"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                        name="floating_phone"
+                        id="floating_phone"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required
+                      />
+                      <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                        Phone number (123-456-7890)
+                      </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                      <input
+                        type="text"
+                        name="floating_company"
+                        id="floating_company"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 peer"
+                        placeholder=" "
+                        required
+                      />
+                      <label className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4  peer-focus:scale-75 peer-focus:-translate-y-6">
+                        Company (Ex. Google)
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure className="h-32">
+                <img src={courseImage} alt="Shoes" className="w-full" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Fashion</div>
+                  <div className="badge badge-outline">Products</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default EnrollClass;
