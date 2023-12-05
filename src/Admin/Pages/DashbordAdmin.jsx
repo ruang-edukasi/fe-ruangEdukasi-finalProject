b3290c503b3e408e68e9ea9c044f54b584521d07
RizkySehan <71442289+RizkySehan@users.noreply.github.com>
2023-12-05 17:39:16 +0700

    Feature iky Api login admin (#25)
    * feature:add Dashbord
    
    * feature: add Layout Dashbord
    
    * Add Layout Dashbord
    
    * feature: Change Layout Reset Password
    
    * feature: Add icons & change Layout sprint 1
    
    * feature: Add Slicing Login & Dashbord Admin
    
    * feature: Add Api Login Admin

---
+++ src/Admin/Pages/DashbordAdmin.jsx
@@ -0,0 +1,11 @@
+import HeaderAdmin from "../Components/Header/HeaderAdmin";
+
+function DashbordAdmin() {
+  return (
+    <div>
+      <HeaderAdmin />
+    </div>
+  );
+}
+
+export default DashbordAdmin;
