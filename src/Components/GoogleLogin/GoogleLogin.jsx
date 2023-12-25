327c4250261d2c50c8ff244d94a5d49fb2b8f172
ilhampraditya <130032787+ilhampraditya@users.noreply.github.com>
2023-12-25 21:31:44 +0700

    integrasi api order course, check coupon , and slicing page payment (#55)
    * Add : Pages
    
    * fixed tailwind
    
    * register page
    
    * add feature: home page
    
    * register page done
    
    * update
    
    * register page done
    
    * add otp page,update register page
    
    * add redux for register
    
    * add google login, add page otp and integration api verif otp
    
    * add sweetalert in page otp n login
    
    * slicing page profile and integration api update profile
    
    * update page profile, integration api update profile & change password
    
    * add integrasi api order course, check coupon , slicing page payment
    
    ---------
    
    Co-authored-by: Ahmads <bariziahnad19@gmail.com>
    Co-authored-by: Ariantika Putri <arin.dila02@gmail.com>

--- src/Components/GoogleLogin/GoogleLogin.jsx
+++ src/Components/GoogleLogin/GoogleLogin.jsx
@@ -1,12 +1,30 @@
 import PropTypes from "prop-types";
-const GoogleLogin = ({ buttonText }) => {
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { registerWithGoogle } from "../../redux/action/authAction";
+
+
+function GoogleLogin ({ buttonText }) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+     const handleRegisGoogle = async (event) => {
+       event.preventDefault();
+       dispatch(registerWithGoogle(navigate));
+     };
+
+
   return (
-    <button className="flex items-center gap-1 text-gray-700 ">
+    <button
+      className="flex items-center gap-1 text-gray-700 "
+      onClick={handleRegisGoogle}
+    >
       <img src="/google.svg" width={26} />
       {buttonText}
     </button>
   );
-};
+}
 
 GoogleLogin.propTypes = {
   buttonText: PropTypes.string,
