01d3f20b0e2f5072c15168216a03e2c91fd42b1f
M Rizky Ramdhani <rizkysehan@gmail.com>
2024-01-02 16:55:06 +0700

    feature: add Api Myclass

--- src/redux/action/courseAction.js
+++ src/redux/action/courseAction.js
@@ -14,6 +14,7 @@ import {
   setCourseDashbord,
   setEnrollMessage,
   setAddprogess,
+  setMyCourseDashboard,
 } from "../reducer/courseReducers";
 import Swal from "sweetalert2";
 import { setToken } from "../reducer/authReducer";
@@ -261,6 +262,50 @@ export const getCourseDashbord = (filters) => async (dispatch) => {
     const { response } = data.data;
 
     dispatch(setCourseDashbord(response));
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      alert(error?.response?.data?.message);
+      return;
+    }
+  }
+};
+
+export const getMyCourseDashboard = (filters) => async (dispatch, getState) => {
+  try {
+    const params = new URLSearchParams();
+
+    if (filters.type && filters.type.length > 0) {
+      filters.type.forEach((typeId) => {
+        params.append("typeId", typeId);
+      });
+    }
+
+    if (filters.category && filters.category.length > 0) {
+      filters.category.forEach((categoryId) => {
+        params.append("catId", categoryId);
+      });
+    }
+
+    if (filters.level && filters.level.length > 0) {
+      filters.level.forEach((levelId) => {
+        params.append("levelId", levelId);
+      });
+    }
+    const apiUrl = `${
+      import.meta.env.VITE_API_URL
+    }/api/v1/user/dashboard/multi?${params.toString()}`;
+
+    let { token } = getState().auth;
+
+    const data = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const { response } = data.data;
+
+    dispatch(setMyCourseDashboard(response?.myCourse));
   } catch (error) {
     if (axios.isAxiosError(error)) {
       alert(error?.response?.data?.message);
